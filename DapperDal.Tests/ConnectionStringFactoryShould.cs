196fdb1ecbf28a3cb11498334308be5d17000abe
Jeffery Cosby <jcoz1968@hotmail.com>
2018-08-12 19:39:47 -0500

    resolving dependency version conflicts

---
+++ DapperDal.Tests/ConnectionStringFactoryShould.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.Common;
+
+namespace DapperDal.Tests
+{
+    [TestClass]
+    public class ConnectionStringFactoryShould
+    {
+        [TestMethod]
+        public void ReturnValidSameStringOnCreate()
+        {
+            //Arrange
+            var connectionString = "Server=server;Database=database;User Id=user;Password=password";
+            var factory = new ConnectionStringFactory(connectionString);
+
+            //Act
+            var result = factory.Create();
+
+            //Assert
+            Assert.AreEqual(connectionString, result);
+        }
+
+        [TestMethod]
+        public void ReturnInvalidIfNotValidSqlConnectionString()
+        {
+            //Arrange
+            var connectionString = "foo";
+            string provider = "System.Data.SqlClient";
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+            string result = "";
+            var ex = new Exception();
+
+            //Act
+            try
+            {
+                var dalfactory = new ConnectionStringFactory(connectionString);
+                result = dalfactory.Create();
+            }
+            catch (System.Exception e)
+            {
+                ex = e;
+            }
+
+            //Assert
+            Assert.AreNotEqual(new Exception(), ex);
+            Assert.AreNotEqual(string.Empty, ex.Message);
+            Assert.AreEqual(string.Empty, result);
+        }
+    }
+}
---
+++ DapperDal.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DapperDal.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DapperDal.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f733d37c-da37-49ac-9ef2-fdd7bde36bbc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
