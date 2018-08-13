ec6dea5f120c3f86b783701cc5905648072d74fa
Jeffery Cosby <jcoz1968@hotmail.com>
2018-08-12 19:51:36 -0500

    ConnectionFactory tests

---
+++ DapperDal.Tests/SqlConnectionFactoryShould.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+
+namespace DapperDal.Tests
+{
+    [TestClass]
+    public class SqlConnectionFactoryShould
+    {
+        [TestMethod]
+        public void ReturnSqlConnection()
+        {
+            //Arrange
+            var connectionString = "Server=server;Database=database;User Id=user;Password=password";
+            var connectionStringFactory = new ConnectionStringFactory(connectionString);
+            var connectionFactory = new SqlConnectionFactory(connectionStringFactory);
+
+            //Act
+            var result = connectionFactory.Create();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(SqlConnection));
+        }
+    }
+}
